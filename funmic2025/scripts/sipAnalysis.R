0f53e5da98a53c9825e5d74ef6214eb67b9cf110
danchurch <danchurchthomas@gmail.com>
2025-03-18 09:43:00 +0100

    debugging sipAnalysis still

--- funmic2025/scripts/sipAnalysis.R
+++ funmic2025/scripts/sipAnalysis.R
@@ -37,25 +37,34 @@ dev.new() ## start a new plotter, so we don't clobber the old figure
 NMS_braycurtis(ps.prop)
 
 
-## let's break up our data by substrate:
-samp.a <- rownames(sample_data(ps)[sample_data(ps)$Substrate == "A",])
-samp.g <- rownames(sample_data(ps)[sample_data(ps)$Substrate == "G",])
+## let's break up our data by substrate, making abundances in each proportional:
+
+## methanol
 samp.m <- rownames(sample_data(ps)[sample_data(ps)$Substrate == "M",])
-ps.a <- prune_samples(samp.a, ps)
-ps.g <- prune_samples(samp.g, ps)
 ps.m <- prune_samples(samp.m, ps)
+ps.m.prop <- transform_sample_counts(ps.m, function(otu) otu/sum(otu))
+
+## glucose
+samp.g <- rownames(sample_data(ps)[sample_data(ps)$Substrate == "G",])
+ps.g <- prune_samples(samp.g, ps)
+ps.g.prop <- transform_sample_counts(ps.g, function(otu) otu/sum(otu))
+
+## acetate
+samp.a <- rownames(sample_data(ps)[sample_data(ps)$Substrate == "A",])
+ps.a <- prune_samples(samp.a, ps)
+ps.a.prop <- transform_sample_counts(ps.a, function(otu) otu/sum(otu))
+
 
 ## and let's look at these with a PCA approach:
 
 dev.new()
-
-plotPCAWithSpecies(ps=ps.m, ptitle="Methanol PCA")
-
-dev.new()
-plotPCAWithSpecies(ps=ps.a, ptitle="Acetate PCA")
+plotPCAWithSpecies(whichPS=ps.m.prop, ptitle="Methanol PCA")
 
 dev.new()
-plotPCAWithSpecies(ps=ps.g, ptitle="Glucose PCA")
+plotPCAWithSpecies(whichPS=ps.a.prop, ptitle="Acetate PCA")
+
+## how would you plot the glucose plot? Try it on your own.
+
 
 ## the PCA and species scoring algorithms are such that the species are positioned close to 
 ## the sites that they are most important in distinguishing. 
@@ -64,16 +73,17 @@ plotPCAWithSpecies(ps=ps.g, ptitle="Glucose PCA")
 ## here is a function that take the ASV name, the phyloseq object, and the title you want to give it:
 
 dev.new()
-getFractionAbundances(ASVname="ASV1", whichPS=ps.m, ptitle="methanol, ASV21")
+getFractionAbundances(ASVname="ASV1", whichPS=ps.m.prop, ptitle="methanol, ASV21")
 
 dev.new()
-getFractionAbundances(ASVname="ASV33", whichPS=ps.g, ptitle="glucose, ASV40")
+getFractionAbundances(ASVname="ASV33", whichPS=ps.g.prop, ptitle="glucose, ASV33")
+
 
 ## that function reports taxonomy. But we can also get it this way, with phyloseq:
 
 tax_table(ps)["ASV21",]
 
-## if you want to save a plot, try the pdf() or png() functions:
+## remember that if you want to save a plot, try the pdf() or png() functions:
 
 png(file="fractionAbundances_ASV33_glucose.png")
 getFractionAbundances(ASVname="ASV33", whichPS=ps.g, ptitle="glucose, ASV33")
