e15da621760698e3b454b493e5c3fbfd7c6219c5
danchurch <danchurchthomas@gmail.com>
2025-03-20 12:33:49 +0100

    fixing sips functions for bd

--- funmic2025/scripts/sipFunctions.R
+++ funmic2025/scripts/sipFunctions.R
@@ -24,30 +24,62 @@ plotPCAWithSpecies <- function (whichPS, scalingType=1, scalingAmount=3, ptitle)
         )
 }
 
-getFractionAbundances = function(ASVname,whichPS,ptitle=""){
+getFractionAbundances = function(ASVname,whichPS,ptitle="",fracOrBD="BD"){
     samp.12 <- rownames(sample_data(whichPS)[sample_data(whichPS)$Isotope == "12",])
     ps.12 <- prune_samples(samp.12, whichPS)
     ps.12.ASV <- prune_taxa(ASVname, ps.12)
-    df12 <- as.data.frame(cbind(sample_data(ps.12.ASV)$Fraction, otu_table(ps.12.ASV)))
+    df12 <- as.data.frame(cbind(sample_data(ps.12.ASV)[,fracOrBD], otu_table(ps.12.ASV)))
     df12[] <- sapply(df12, as.numeric)
-    colnames(df12) <- c("Fraction","Abundance")
+    colnames(df12) <- c(fracOrBD,"Abundance")
     df12$Isotope <- 12
     ## repeat for 13 isos
     samp.13 <- rownames(sample_data(whichPS)[sample_data(whichPS)$Isotope == "13",])
     ps.13 <- prune_samples(samp.13, whichPS)
     ps.13.ASV <- prune_taxa(ASVname, ps.13)
-    df13 <- as.data.frame(cbind(sample_data(ps.13.ASV)$Fraction, otu_table(ps.13.ASV)))
+    df13 <- as.data.frame(cbind(sample_data(ps.13.ASV)[,fracOrBD], otu_table(ps.13.ASV)))
     df13[] <- sapply(df13, as.numeric)
-    colnames(df13) <- c("Fraction","Abundance")
+    colnames(df13) <- c(fracOrBD,"Abundance")
     df13$Isotope <- 13
     df12_13 <- rbind(df12,df13)
     df12_13$Isotope <- as.factor(df12_13$Isotope)
     print(tax_table(ps.12.ASV))
     ## how would we do this with ggplot?
+    if(fracOrBD == "BD"){
+    ggplot(df12_13, aes(x=BD, y=Abundance, color=Isotope, shape=Isotope)) +
+      geom_point(size=4) +
+      geom_smooth(se=FALSE, fullrange=FALSE, linetype="dashed") + ggtitle(ptitle)
+    } else if (fracOrBD == "Fraction"){
     ggplot(df12_13, aes(x=Fraction, y=Abundance, color=Isotope, shape=Isotope)) +
       geom_point(size=4) +
       geom_smooth(se=FALSE, fullrange=FALSE, linetype="dashed") + ggtitle(ptitle)
+    }
 }
+
+### old #########
+#getFractionAbundances = function(ASVname,whichPS,ptitle="",fracOrBD="BD"){
+#    samp.12 <- rownames(sample_data(whichPS)[sample_data(whichPS)$Isotope == "12",])
+#    ps.12 <- prune_samples(samp.12, whichPS)
+#    ps.12.ASV <- prune_taxa(ASVname, ps.12)
+#    df12 <- as.data.frame(cbind(sample_data(ps.12.ASV)$Fraction, otu_table(ps.12.ASV)))
+#    df12[] <- sapply(df12, as.numeric)
+#    colnames(df12) <- c("Fraction","Abundance")
+#    df12$Isotope <- 12
+#    ## repeat for 13 isos
+#    samp.13 <- rownames(sample_data(whichPS)[sample_data(whichPS)$Isotope == "13",])
+#    ps.13 <- prune_samples(samp.13, whichPS)
+#    ps.13.ASV <- prune_taxa(ASVname, ps.13)
+#    df13 <- as.data.frame(cbind(sample_data(ps.13.ASV)$Fraction, otu_table(ps.13.ASV)))
+#    df13[] <- sapply(df13, as.numeric)
+#    colnames(df13) <- c("Fraction","Abundance")
+#    df13$Isotope <- 13
+#    df12_13 <- rbind(df12,df13)
+#    df12_13$Isotope <- as.factor(df12_13$Isotope)
+#    print(tax_table(ps.12.ASV))
+#    ## how would we do this with ggplot?
+#    ggplot(df12_13, aes(x=Fraction, y=Abundance, color=Isotope, shape=Isotope)) +
+#      geom_point(size=4) +
+#      geom_smooth(se=FALSE, fullrange=FALSE, linetype="dashed") + ggtitle(ptitle)
+#}
 
 plotFamilies = function(howManyASVs=30){
     topX <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:howManyASVs]
