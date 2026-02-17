4f8c978044f8e5f3e65ed79ca5889ee4e559516d
meierdv <67641585+meierdv@users.noreply.github.com>
2026-02-17 12:37:19 +0100

    Add files via upload

--- funmic2026/getKelpReads.sh
+++ funmic2026/getKelpReads.sh
@@ -1,10 +1,38 @@
-names=(
+Read_set_list=(
 "ERR3801502"
 "ERR3801542"
 "ERR3801603"
 )
 
-for i in ${names[@]}; do
-  prefetch $i
-  fasterq-dump --split-files $i
+# Download reads from NCBI
+
+for READ_SET in ${Read_set_list[@]};
+
+do
+  fasterq-dump \
+    --progress \
+    --split-files \
+    --skip-technical \
+    --outdir ${READ_SET} \
+    ${READ_SET}
+done
+
+
+# Do a quick quality control of the reads (99% of reads are retained)
+
+for READ_SET in ${Read_set_list[@]};
+
+do
+  bbduk.sh \
+    threads=12 \
+    ref=adapters,artifacts,phix \
+    qtrim=rl \
+    trimq=10 \
+    maxns=0 \
+    in=${READ_SET}/${READ_SET}_1.fastq \
+    in2=${READ_SET}/${READ_SET}_2.fastq \
+    out=${READ_SET}_1.fastq \
+    out2=${READ_SET}_2.fastq \
+    overwrite=T
+
 done
