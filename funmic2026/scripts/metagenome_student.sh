167c8f189ce959c1299f451f8e06a62b3fb31abf
dimitri <dimitri.v.meier@gmail.com>
2026-02-24 09:56:39 +0100

    updt

--- funmic2026/scripts/metagenome_student.sh
+++ funmic2026/scripts/metagenome_student.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-#Hello course!#
+#### Metagenome analysis #######
+
+
+## In this course we are analyzing DNA shotgun sequencing data from the following publication:
+## paper: Van Erk et al. 2021, doi.org/mgt4
+## dataset accession number on NCBI/ENA: PRJEB36085
+
+ssh deNBI
+
+## First, create a folder  for all the new files that you will generate during the analysis.
+## We will keep it organized and structured to easily find the output produced by the different tools
+
+# Make a new directory "Kelp" under /vol/funmic/
+mkdir /vol/funmic/Kelp
